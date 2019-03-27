896a1b3b2b9017b69e7362f4629fbfb571dd6cc9
Natan <natan.derroitte@gmail.com>
2019-03-27 16:56:26 +0100

    vio c'est de lo

--- main.m
+++ main.m
@@ -4,16 +4,12 @@ function main()
 
     %
     disp('Program started');
-    % Use the following line if you had to recompile remoteApi
-    %vrep = remApi('remoteApi', 'extApi.h');
+    % Launching vrep 
     vrep = remApi('remoteApi');
     % End the last simulation if not already done
     vrep.simxFinish(-1);
+    % Creating the id for vrep
     id = vrep.simxStart('127.0.0.1', 19997, true, true, 2000, 5);
-    
-    % If you get an error like: 
-    %   Remote API function call returned with error code: 64. Explanation: simxStart was not yet called.
-    % Make sure your code is within a function! You cannot call V-REP from a script. 
 
     if id < 0
         disp('Failed connecting to remote API server. Exiting.');
@@ -25,6 +21,8 @@ function main()
     % Make sure we close the connection whenever the script is interrupted.
     cleanupObj = onCleanup(@() cleanup_vrep(vrep, id));
 
-    % This will only work in "continuous remote API server service". 
-    % See http://www.v-rep.eu/helpFiles/en/remoteApiServerSide.htm
+    % Start simulation
     vrep.simxStartSimulation(id, vrep.simx_opmode_oneshot_wait);
+    
+    
+    vrep.simxFinish(-1);
